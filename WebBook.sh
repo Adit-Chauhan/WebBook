05c2cde87ecde0b1a17f877f887cc932180fdde3
Adit Chauhan <chauhan.adit.98@gmail.com>
2021-11-06 12:10:48 +0530

    Added arbitary input support

--- WebBook.sh
+++ WebBook.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 FILE="$HOME/.config/bookmarks.conf"
-
+WEBOOK_MODE_VAR="NOARGOMG"
+WEBOOK_MODE_VAR_TYPE=""
+WEBOOK_MODE_VAR_DEF="NOARGOMG"
 FF() {
-	#printf "Running firefox $@\n"
+	printf "Running firefox $@\n"
 	firefox $@ &
 	disown
 }
@@ -20,9 +22,9 @@ roof() {
 }
 
 rofied() {
-	#printf "\n\nIn Rofied\n"
-	#printf $(echo "$@" | awk -F'::' '{print $1}')
-	#printf "\n"
+	printf "\n\nIn Rofied\n"
+	printf $(echo "$@" | awk -F'::' '{print $1}')
+	printf "\n"
 	RofiOut=$(echo "$@" | awk -F'::' '{print $1}' | rofi -dmenu)
 	if [ "$RofiOut" == "" ]; then
 		echo "Quitting"
@@ -42,49 +44,95 @@ filterFunction() {
 }
 
 JUMPROTO() {
-	#printf "\n\nJumpProto Jump to $1"
+	printf "\n\nJumpProto Jump to $1\n"
 	Sec=$(filterSection $1)
-	#printf "\nNew Section \n$Sec\n"
-	#printf "Selection \n$(echo "$Sec" | awk -F'::' '{print $1}')\n"
+	setmodevar $1
+	printf "\nNew Section \n$Sec\n"
+	printf "Selection \n$(echo "$Sec" | awk -F'::' '{print $1}')\n"
 	roof_inp="$(echo "$Sec" | awk -F'::' '{print $1}')"
 	sel=$(roof "$roof_inp")
-	#printf "\nSelected \n$sel"
-	OpDecode $(echo "$Sec" | grep "$sel" | sed 's/.*:://')
+	printf "\nSelected \n$sel"
+	if [ "$sel" != "" ]; then
+		OpDecode "$(echo "$Sec" | grep "$sel" | sed 's/.*:://')" "$sel"
+	fi
 }
 
 CUSTOM() {
 	if grep -q "SUB $1" $FILE; then
-		#printf "Found Subsitutuion $1\n$(grep "SUB $1" $FILE)\n"
+		printf "Found Subsitutuion $1\n$(grep "SUB $1" $FILE)\n"
 		link=$(sed -n "s/SUB $1//p" $FILE | sed "s,{},$2,g")
 		FF $link
 	else
-		rofied "Function\nNot\nDefined"
-		main
+		fallback_func
 	fi
 }
 
-OpDecode() {
-	OP=$(echo "$@" | awk -F' ' '{print $1}')
-	ARG=$(echo "$@" | awk -F' ' '{print $2}')
-	#printf "\n OpDecode\nOP $OP\nARG $ARG\n"
+fallback_func() {
+	rofied "Function\nNot\nDefined"
+	main
+}
 
-	case $OP in
-		"") ;;
-		\;*) ;;
-		\"*)
-			Temp=$(echo $OP | sed 's/"//g')
-			FF $Temp
-			;;
-		RUN)
-			FF $ARG
-			;;
-		JUMP)
-			JUMPROTO $ARG
-			;;
-		*)
-			CUSTOM $OP $ARG
-			;;
-	esac
+setmodevar() {
+	printf "setting mode var\n"
+	LINE=$(sed -n "s/| $1.*\[\(.*\)\].*/\1/p" $FILE)
+	printf "Line $LINE\n"
+	if [ "$LINE" != "" ]; then
+		if grep -q "SUB $LINE" $FILE; then
+			WEBOOK_MODE_VAR_TYPE="SUB"
+			WEBOOK_MODE_VAR=$LINE
+		elif grep -q "EXE $LINE" $FILE; then
+			WEBOOK_MODE_VAR_TYPE="EXE"
+			WEBOOK_MODE_VAR=$(sed -n "s/EXE $LINE \(.*\)/\1/p" $FILE)
+		fi
+	else
+		WEBOOK_MODE_VAR=$WEBOOK_MODE_VAR_DEF
+	fi
+	printf "MODE VARS $WEBOOK_MODE_VAR $WEBOOK_MODE_VAR_TYPE\n"
+}
+
+moderunner() {
+	printf "Entered Mode runner\n"
+	printf "Mode Runner Arc $@\n"
+	if [ $WEBOOK_MODE_VAR != $WEBOOK_MODE_VAR_DEF ]; then
+		case $WEBOOK_MODE_VAR_TYPE in
+			"") ;;
+			SUB)
+				link=$(sed -n "s/SUB $WEBOOK_MODE_VAR//p" $FILE | sed "s,{},$@,g")
+				FF $link
+				;;
+			EXE)
+				$WEBOOK_MODE_VAR $1
+				;;
+		esac
+	fi
+
+}
+
+OpDecode() {
+	printf "OpDecode \$1 == $1, \$2 == $2\n"
+	if [ "$1" == "" ]; then
+		moderunner "$2"
+	else
+		OP=$(echo "$1" | awk -F' ' '{print $1}')
+		ARG=$(echo "$1" | awk -F' ' '{print $2}')
+		printf "\n OpDecode\nOP $OP\nARG $ARG\n"
+		case $OP in
+			"") ;;
+			\;*) ;;
+			\"*)
+				FF $(echo $OP | sed 's/"//g')
+				;;
+			RUN)
+				FF $ARG
+				;;
+			JUMP)
+				JUMPROTO $ARG
+				;;
+			*)
+				CUSTOM $OP $ARG
+				;;
+		esac
+	fi
 }
 
 main() {
